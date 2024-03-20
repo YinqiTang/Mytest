d49a022dee8a174d8291d6515a1b73ff38e2813f
Yinqi Tang <yinqi.tang@agreena.com>
2024-03-20 16:41:37 +0100

    Initial commit

---
+++ R/mysum.R
@@ -0,0 +1,12 @@
+#' Title
+#'
+#' @param x Variable 1
+#' @param y Variable 2
+#'
+#' @return sum of x and y
+#' @export
+#'
+mysum <- function(x, y) {
+  z <- x + y
+  return(z)
+}
---
+++ R/mytest.R
@@ -0,0 +1,4 @@
+mytest <- function(x, y){
+  z <- x * y
+  return(z)
+}
